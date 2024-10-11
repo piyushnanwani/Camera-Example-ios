6ad305d3fd3dab7980de5540ee80885509c03efe
Piyush Nanwani <pnanwani61@gmail.com>
2024-10-11 15:15:47 +0530

    Initial Commit

---
+++ Camera Example 2/Camera_Example_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_Example_2App.swift
+//  Camera Example 2
+//
+//  Created by Piyush Nanwani on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Camera_Example_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
